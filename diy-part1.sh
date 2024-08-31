e91e78b94e4b13b1426b6f54158c1d66b6b0ea88
liedieyou <76842456+liedieyou@users.noreply.github.com>
2024-08-31 10:17:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/liedieyou/OpenWrt-Packages" >> feeds.conf.default
